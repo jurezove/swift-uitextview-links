334ab02cdda956a383491a48dd51872b236e8f9e
Jure Zove <jure.pase.kure@gmail.com>
2016-04-24 20:17:51 +0200

    Initial Commit

---
+++ Hyperlinks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hyperlinks
+//
+//  Created by Jure Zove on 24/04/16.
+//  Copyright © 2016 Candy Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
